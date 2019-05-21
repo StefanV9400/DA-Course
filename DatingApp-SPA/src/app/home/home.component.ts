e56c91cbbd411665535df6366069d71338a09d45
StefanV <S94.Vasilev@gmail.com>
2019-05-21 14:09:13 +0200

    end of section six

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/Http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
